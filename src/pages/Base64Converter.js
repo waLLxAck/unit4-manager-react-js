d652a2d3bcd8896b992cb6277c04274fd2d40c2d
Svilen Petrov <svilen.petrov97@gmail.com>
2022-08-15 22:48:33 +0100

    adding file to base64

--- src/pages/Base64Converter.js
+++ src/pages/Base64Converter.js
@@ -10,6 +10,7 @@ const Base64Converter = () => {
     const [fileType, setFileType] = React.useState("");
     const [fileExtension, setFileExtension] = React.useState("");
     const [supportedFileTypes, setSupportedFileTypes] = React.useState([]);
+    const [base64StringToConvert, setBase64StringToConvert] = React.useState("");
 
     // usememo for signatures
     const signatures = React.useMemo(() => {
@@ -103,7 +104,7 @@ const Base64Converter = () => {
     return (
         <div className="container">
             {/* bootstrap page layout */}
-            <div className="row">
+            <div className="row mb-4">
                 <div className="col-12">
                     <h4>Base64 to file</h4>
                     <p>Convert base64 to file. Supported file types: {supportedFileTypes.join(", ")}.</p>
@@ -125,24 +126,17 @@ const Base64Converter = () => {
                             <input type="text" className="form-control" id="file-extension" value={fileExtension} disabled />
                         </div>
                     </div>
-                    <div className="row mt-2">
+                    <div className="row mt-3">
                         <div className="col-auto">
                             <Button variant="success" onClick={() => {
                                 // convert base64 string to file
                                 const file = base64StringtoFile();
 
-                                console.log(file);
-
                                 // download file
                                 var file_url = URL.createObjectURL(file);
                                 var file_link = document.createElement("a");
-
-                                console.log(file_url);
-                                console.log(file_link);
-
                                 file_link.href = file_url;
                                 file_link.download = file.name;
-                                console.log(file.name);
                                 file_link.click();
                             }}>Decode to file</Button>
                         </div>
@@ -155,7 +149,34 @@ const Base64Converter = () => {
                             }}>Clear</Button>
                         </div>
                     </div>
-
+                </div>
+            </div>
+            <hr />
+            <div className="row">
+                <div className="col-12">
+                    <h4>File to Base64</h4>
+                    <p>Convert file to base64.</p>
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="form-group">
+                                <input type="file" className="form-control" id="file" onChange={(e) => {
+                                    // get file
+                                    const file = e.target.files[0];
+                                    // get file reader
+                                    const reader = new FileReader();
+                                    // set base64 string
+                                    reader.onload = function (e) {
+                                        setBase64StringToConvert(e.target.result.split(",")[1]);
+                                    }
+                                    // read file
+                                    reader.readAsDataURL(file);
+                                }
+                                } />
+                                <small id="fileHelp" className="form-text text-muted">Select a file to convert to base64.</small>
+                                <textarea className="form-control mt-3" id="base64" rows="3" value={base64StringToConvert} onChange={(e) => setBase64StringToConvert(e.target.value)}></textarea>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
